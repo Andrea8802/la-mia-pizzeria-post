c3dc1c2de895ef281f89c267fcaa9b6e3c87e4b3
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-05-04 15:12:44 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    { 
+
+    public IActionResult Index()
+        {
+            using(PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizze = db.Pizza.OrderBy(pizza => pizza.Id).ToList<Pizza>();
+
+                if (pizze.Count == 0)
+                    return View("Error", "Non ci sono pizze!");
+
+                return View(pizze);
+            }
+
+        }
+
+    public IActionResult Details(long id)
+    {
+            using(PizzaContext db = new PizzaContext())
+            {
+                Pizza pizza = db.Pizza.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if (pizza == null)
+                    return View("Error", "Nessuna pizza trovata con questo ID!");
+
+                return View(pizza);
+            }
+
+        }
+    }
+}
---
+++ la-mia-pizzeria-static/Pizza.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static
+{
+    [Table("pizza")]
+    public class Pizza
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public decimal Prezzo { get; set; }
+        public string Img { get; set; }
+
+    }
+}
